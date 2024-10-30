0975937203e6e9424600bd8c537b1e5204db3718
Kodai Miyazaki / Soynyuu <miyazaki.kodai@outlook.com>
2024-10-30 10:38:30 +0900

    fix

--- lib/screens/address_book_screen.dart
+++ lib/screens/address_book_screen.dart
@@ -1,3 +1,4 @@
+//とりあえず機能は実装せず、ハリボテで
 import 'package:flutter/material.dart';
 
 class AddressBookScreen extends StatelessWidget {
@@ -6,9 +7,9 @@ class AddressBookScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<Map<String, String>> addressBook = [
-      {'name': '山田 太郎', 'address': '東京都新宿区1-1-1'},
-      {'name': '鈴木 次郎', 'address': '東京都渋谷区2-2-2'},
-      {'name': '佐藤 花子', 'address': '東京都港区3-3-3'},
+      {'name': '山田 太郎'},
+      {'name': '鈴木 次郎'},
+      {'name': '佐藤 花子'},
     ];
 
     return Scaffold(
@@ -20,7 +21,6 @@ class AddressBookScreen extends StatelessWidget {
         itemBuilder: (context, index) {
           return ListTile(
             title: Text(addressBook[index]['name']!),
-            subtitle: Text(addressBook[index]['address']!),
           );
         },
       ),
