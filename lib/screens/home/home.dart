caf3fb46306bd24cea9931ff22c2b790c5b9440a
Kodai.M|Soynyuu <miyazaki.kodai@outlook.com>
2025-01-02 00:04:35 +0900

    ボタンのアニメーションをオフに

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,22 +8,32 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: TextButton(
-            onPressed: () {},
-            child: RichText(
-              text: TextSpan(children: [
-                TextSpan(
-                    text: "手紙\nを",
-                    style: GoogleFonts.sawarabiMincho(
-                        fontSize: 30,
-                        color: Color(0xFF8F8F8F),
-                        decoration: TextDecoration.underline)),
-                TextSpan(
-                    text: "\nかく",
-                    style: GoogleFonts.sawarabiMincho(
-                        fontSize: 36,
-                        color: Color(0xFF542E00),
-                        decoration: TextDecoration.underline)),
-              ]),
-            )));
+      style: ButtonStyle(
+        overlayColor: MaterialStateProperty.all(Colors.transparent),
+      ),
+      onPressed: () {},
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: "手紙\nを",
+              style: GoogleFonts.sawarabiMincho(
+                fontSize: 30,
+                color: Color(0xFF8F8F8F),
+                decoration: TextDecoration.underline,
+              ),
+            ),
+            TextSpan(
+              text: "\nかく",
+              style: GoogleFonts.sawarabiMincho(
+                fontSize: 36,
+                color: Color(0xFF542E00),
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
